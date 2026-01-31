88f24837635e580fc80ec72a7b8113cbd35c2006
MSAP <diqa.a.pohan@gmail.com>
2026-01-31 15:19:15 +0700

    Add Main scene; update sounds & scenes
    Add new Game/Main.tscn and import metadata. Replace/rename step audio assets by adding Step_grass_1_(1), Step_grass_2_(1), Step_stone_1_(1), Step_stone_2_(1) (with .import files) and remove the old Step_grass_1 asset. Remove several forestBackground PNGs and their import metadata. Also apply modifications to existing scenes and scripts (Game/Region3.tscn, Game/Scene/main_menu.tscn, Game/Scene/player.tscn, Game/Script/StateMachine/PlayerStates/player_run.gd, Game/Script/player.gd) and update default_bus_layout.tres.

--- Game/Script/StateMachine/PlayerStates/player_run.gd
+++ Game/Script/StateMachine/PlayerStates/player_run.gd
@@ -3,14 +3,27 @@ extends State
 const SPEED = 120
 const ACCELERATE = 5
 
+# --- SETUP TIMER SUARA ---
+var footstep_timer : float = 0.0
+@export var footstep_delay : float = 0.25 # Atur angka ini (0.3 - 0.4 biasanya pas)
+
 func UpdatePhysics(delta: float):
 	super.UpdatePhysics(delta)
 	
 	character.velocity = character.velocity.lerp(character.inputDirection * SPEED, ACCELERATE * delta)
 	character.move_and_slide()
 	
-func Update():
+	# LOGIKA SUARA KAKI PAKE TIMER
+	if character.velocity.length() > 10:
+		footstep_timer -= delta
+		if footstep_timer <= 0:
+			character.play_footstep()
+			footstep_timer = footstep_delay # Reset jeda suara
+	else:
+		# Reset timer kalau berhenti, supaya pas jalan lagi langsung bunyi
+		footstep_timer = 0 
 	
+func Update():
 	character.UpdateAnimation()
 	if character.inputDirection == Vector2.ZERO:
 		parentStateMachine.SwitchTo("Idle")
--- Game/Script/player.gd
+++ Game/Script/player.gd
@@ -1,8 +1,47 @@
 extends BaseCharacter
 
+# --- SETUP AUDIO ---
+@onready var sfx_run: AudioStreamPlayer2D = $SfxRun
+
+# List suara (Pastiin file .wav lu ada di folder ini ya!)
+var sfx_grass_collection = [
+	preload("res://Assets/Sounds/Step_grass_1_(1).mp3.wav"),
+	preload("res://Assets/Sounds/Step_grass_2_(1).mp3.wav")
+]
+
+var sfx_stone_collection = [
+	preload("res://Assets/Sounds/Step_stone_1_(1).mp3.wav"),
+	preload("res://Assets/Sounds/Step_stone_2_(1).mp3.wav")
+]
+
+var current_surface: String = "grass"
+
 func _ready() -> void:
+	# super._ready() penting kalo di BaseCharacter ada kodingan di func _ready
 	pass
 
 func _process(_delta: float) -> void:
-	
+	# LOGIKA GERAK LU YANG LAMA (Jangan sampe ilang!)
 	inputDirection = Input.get_vector("Left", "Right", "Up", "Down")
+
+# Fungsi buat dipanggil dari state player_run.gd
+func play_footstep():
+	var selected_stream : AudioStream
+	
+	if current_surface == "stone":
+		selected_stream = sfx_stone_collection.pick_random()
+	else:
+		selected_stream = sfx_grass_collection.pick_random()
+	
+	sfx_run.stream = selected_stream
+	sfx_run.pitch_scale = randf_range(0.8, 1.2)
+	sfx_run.play()
+
+# --- SIGNAL DETEKSI AREA ---
+func _on_surface_detector_area_entered(area: Area2D) -> void:
+	if area.is_in_group("StoneRegion"):
+		current_surface = "stone"
+
+func _on_surface_detector_area_exited(area: Area2D) -> void:
+	if area.is_in_group("StoneRegion"):
+		current_surface = "grass"
